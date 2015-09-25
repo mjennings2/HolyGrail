66efc54eb01b2bc2d062e420d2a93404185f033b
jmeyer7265 <jmeyer@bayviewglen.ca>
2015-09-24 21:50:51 -0400

    added board x and y coordinates

--- Holy Grail/src/com/bayviewglen/something/Board.java
+++ Holy Grail/src/com/bayviewglen/something/Board.java
@@ -3,7 +3,81 @@ package com.bayviewglen.something;
 public class Board {
 
 	public Board() {
-		// TODO Auto-generated constructor stub
+		Square[] squares = new Square[72]; 
+		squares[0]= new Square (756,321);
+		squares[1]= new Square (732,321);
+		squares[2]= new Square (709,321);
+		squares[3]= new Square (658,321);
+		squares[4]= new Square (658,296);
+		squares[5]= new Square (658,272);
+		squares[6]= new Square (658,246);
+		squares[7]= new Square (658,222);
+		squares[8]= new Square (658,200);
+		squares[9]= new Square (658,173);
+		squares[10]= new Square (658,151);
+		squares[11]= new Square (658,125);
+		squares[12]= new Square (662,125);
+		squares[13]= new Square (640,125);
+		squares[14]= new Square (616,125);
+		squares[15]= new Square (593,125);
+		squares[16]= new Square (570,125);
+		squares[17]= new Square (546,125);
+		squares[18]= new Square (523,125);
+		squares[19]= new Square (500,125);
+		squares[20]= new Square (477,125);
+		squares[21]= new Square (454,125);
+		squares[22]= new Square (453,150);
+		squares[23]= new Square (493,174);
+		squares[24]= new Square (493,198);
+		squares[25]= new Square (493,223);
+		squares[26]= new Square (493,246);
+		squares[27]= new Square (493,272);
+		squares[28]= new Square (430,272);
+		squares[29]= new Square (408,272);
+		squares[30]= new Square (383,272);
+		squares[31]= new Square (383,296);
+		squares[32]= new Square (383,392);
+		squares[33]= new Square (383,416);
+		squares[34]= new Square (383,442);
+		squares[35]= new Square (383,465);
+		squares[36]= new Square (360,465);
+		squares[37]= new Square (337,465);
+		squares[38]= new Square (313,465);
+		squares[39]= new Square (291,465);
+		squares[40]= new Square (291,442);
+		squares[41]= new Square (291,417);
+		squares[42]= new Square (383,246);
+		squares[43]= new Square (383,221);
+		squares[44]= new Square (383,197);
+		squares[45]= new Square (383,173);
+		squares[46]= new Square (384,149);
+		squares[47]= new Square (384,124);
+		squares[48]= new Square (361,124);
+		squares[49]= new Square (337,124);
+		squares[50]= new Square (314,124);
+		squares[51]= new Square (292,124);
+		squares[52]= new Square (291,150);
+		squares[53]= new Square (291,171);
+		squares[54]= new Square (291,196);
+		squares[55]= new Square (291,220);
+		squares[56]= new Square (291,224);
+		squares[57]= new Square (291,272);
+		squares[58]= new Square (291,295);
+		squares[59]= new Square (291,318);
+		squares[60]= new Square (291,343);
+		squares[61]= new Square (291,367);
+		squares[62]= new Square (268,392);
+		squares[63]= new Square (244,392);
+		squares[64]= new Square (222,392);
+		squares[65]= new Square (244,392);
+		squares[66]= new Square (222,392);
+		squares[67]= new Square (197,392);
+		squares[68]= new Square (175,392);
+		squares[69]= new Square (150,392);
+		squares[70]= new Square (129,392);
+		squares[71]= new Square (71,392);
+		squares[72]= new Square (814,371);
+		
 	}
 
 }
